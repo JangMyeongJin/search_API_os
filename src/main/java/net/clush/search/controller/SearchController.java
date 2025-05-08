7f1aeae51a325bed557fa40b467fff67ef764c29
JangMyeongJin <jean9710@naver.com>
2025-05-08 16:57:10 +0900

    searchQueryImpl 추가, searchForm 추가

--- src/main/java/net/clush/search/controller/SearchController.java
+++ src/main/java/net/clush/search/controller/SearchController.java
@@ -9,15 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import net.clush.search.dto.SearchRequest;
 import net.clush.search.dto.SearchResponse;
+import net.clush.search.service.SearchService;
 
 @RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchController {
 	
+	private final SearchService searchService;
+	
 	@GetMapping
     public ResponseEntity<SearchResponse> search(@ModelAttribute SearchRequest request) {
-        return ResponseEntity.ok(searchService.search(request));
+        return ResponseEntity.ok(searchService.SearchQuery(request));
     }
 
 }
